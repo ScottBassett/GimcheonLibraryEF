f68a40704b98765768f244d08534779512ecc046
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-11-09 12:16:28 +0000

    Added models and DBContext class

---
+++ GimcheonLibraryEF.DataAccess/GimcheonLibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GimcheonLibraryEF.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GimcheonLibraryEF.DataAccess
+{
+    public class GimcheonLibraryDbContext : DbContext
+    {
+        public GimcheonLibraryDbContext(DbContextOptions<GimcheonLibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
---
+++ GimcheonLibraryEF.DataAccess/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimcheonLibraryEF.DataAccess.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Total Copies")]
+        public int TotalCopies { get; set; }
+
+        [Required]
+        [Display(Name = "Available Copies")]
+        public int AvailableCopies { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
