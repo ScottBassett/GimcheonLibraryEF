96e171affab2f90fd5be462eec8e6bfb281688f4
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-11-30 10:26:47 +0000

    Added BorrowedBooks model

---
+++ GimcheonLibraryEF.DataAccess/Models/BorrowedBooks.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GimcheonLibraryEF.DataAccess.Models
+{
+    public class BorrowedBooks
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+    }
+}
